18d9ee14ce438d1d51515a20f04b23398261bc13
Leyser Pinto <59316466+LeyserPinto@users.noreply.github.com>
2024-01-15 22:04:01 -0300

    :tada: Djando Admin - Models

--- classroom/blog/admin.py
+++ classroom/blog/admin.py
@@ -2,3 +2,11 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Person
+
+class PersonAdmin(admin.ModelAdmin):
+    list_display        = ("first_name", "last_name")
+    list_filter         = ("first_name", "last_name")
+    search_fields       = ["first_name", "last_name"]
+    prepopulated_fields = {"last_name": ("last_name",)}
+
+admin.site.register(Person, PersonAdmin)
